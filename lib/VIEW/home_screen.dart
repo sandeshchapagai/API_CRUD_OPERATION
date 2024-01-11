110f9474ba2a4a6790900046d23e459e02a59f34
Sandy <sandeshchapagai@users.noreply.github.com>
2024-01-11 09:27:14 +0545

    fetch when click viewall button

--- lib/VIEW/home_screen.dart
+++ lib/VIEW/home_screen.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Spacer(),
                 TextButton(
                   onPressed: () {
-                    // fetchdata();
+                    fetchdata();
 
                   },
 
